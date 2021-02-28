36768551a1b074df4956a47d9341aa848c8cafdb
Taffer <chrish@pobox.com>
2021-02-28 07:45:40 -0500

    Success using double sprite technique!

--- rotate-sprite/main.lua
+++ rotate-sprite/main.lua
@@ -11,7 +11,8 @@ gameResources = {
 
 -- Current state of the game.
 gameState = {
-    tick = 0,
+    dy = 0, -- Animated texture y offset.
+    max_y = 0, -- Max dy before we have to reset it to 0.
 
     sprite1_x = 100, -- Draw the normal sprite here.
     sprite1_y = 100,
@@ -28,6 +29,9 @@ function love.load()
     local gameResources = gameResources
     gameResources.images.robot = love.graphics.newImage('resources/character_robot_jump.png')
     gameResources.images.robot2y = love.graphics.newImage('resources/character_robot_jump-2y.png')
+
+    local gameState = gameState
+    gameState.max_y = gameResources.images.robot2y:getHeight() / 2
 end
 
 function love.draw()
@@ -38,14 +42,21 @@ function love.draw()
     love.graphics.draw(gameResources.images.robot, gameState.sprite1_x, gameState.sprite1_y)
 
     -- Animate the texture.
-    local quad = love.graphics.newQuad(0, 0,
+    -- Is using a quad's viewport better than making a new quad?
+    local quad = love.graphics.newQuad(0, 0 + gameState.dy,
         gameResources.images.robot2y:getWidth(), gameResources.images.robot2y:getHeight() / 2,
         gameResources.images.robot2y:getWidth(), gameResources.images.robot2y:getHeight())
     love.graphics.draw(gameResources.images.robot2y, quad, gameState.sprite2_x, gameState.sprite2_y)
 end
 
 function love.update(dt)
+    -- Rotate the sprite one pixel per 1/60 seconds.
+    local gameState = gameState
 
+    gameState.dy = gameState.dy + dt * 60
+    if gameState.dy > gameState.max_y then
+        gameState.dy = 0
+    end
 end
 
 -- Event generation.
