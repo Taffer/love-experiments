482b5d6b73358ef3d21bee670d3387d8c68faa54
Taffer <chrish@pobox.com>
2021-02-28 11:41:21 -0500

    Now with a rectangle around the text area.

--- text-monospaced/main.lua
+++ text-monospaced/main.lua
@@ -19,11 +19,14 @@ gameResources = {
 
 -- Current state of the game.
 gameState = {
-    buff = {}, -- Individual lines of text.
-    buff_start = 1, -- Which line to start drawing at.
-    max_lines = 5, -- Maximum number of lines to display.
+    buff = {},        -- Individual lines of text.
+    buff_start = 1,   -- Which line to start drawing at.
+    max_lines = 5,    -- Maximum number of lines to display.
+    max_columns = 20, -- Maximum number of columns to display.
+    margin = 1,       -- Pixels of margin between the text and its rectangle.
 
-    dy = 16, -- Height of the font.
+    dy = 0, -- Height of the font.
+    dx = 0, -- "Em" width of the font (all characters, as it's monospaced).
 
     text_idx = 1 -- Index into the text resource.
 }
@@ -35,10 +38,17 @@ function love.load()
 
     local gameResources = gameResources
     gameResources.fonts.mono = love.graphics.newFont('resources/LiberationMono-Bold.ttf', 16)
+
+    local gameState = gameState
+    gameState.dy = gameResources.fonts.mono:getHeight()
+    gameState.dx = gameResources.fonts.mono:getWidth('M')
 end
 
 function love.draw()
     local gameResources = gameResources
+
+    local text_x = 100 -- Location of the text area.
+    local text_y = 100
 
     love.graphics.setColor(0, 0, 0, 1) -- black
     love.graphics.clear()
@@ -47,13 +57,18 @@ function love.draw()
     love.graphics.setFont(gameResources.fonts.mono)
     love.graphics.print('Press [Space] to add text.', 10, 10)
 
+    -- Draw a rectangle so we can see if we go over the text area.
+    local gameState = gameState
+    love.graphics.setColor(0, 1, 0, 1) -- green
+    love.graphics.rectangle("line", text_x - gameState.margin, text_y - gameState.margin,
+        gameState.dx * gameState.max_columns + gameState.margin * 2, gameState.dy * gameState.max_lines + gameState.margin * 2)
+
     -- Draw the buffer.
     love.graphics.setColor(1, 1, 1, 1) -- white
 
-    local gameState = gameState
     local delta = 0
     for idx = gameState.buff_start, math.min(#gameState.buff, gameState.buff_start + gameState.max_lines) do
-        love.graphics.print(gameState.buff[idx], 100, 100 + delta)
+        love.graphics.print(gameState.buff[idx], text_x, text_y + delta)
         delta = delta + gameState.dy
     end
 end
