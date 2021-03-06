3596b18fcde092ac896630bd72de2dc578ff1efb
Taffer <chrish@pobox.com>
2021-03-06 09:19:24 -0500

    Use proper identifier

--- image-fades/conf.lua
+++ image-fades/conf.lua
@@ -6,11 +6,11 @@
 function love.conf(t)
     -- "Native" window size matches the Switch. Some day, I'd like to port
     -- things there.
-    t.window.title = "Expriment 5 - Image Fades"
+    t.window.title = 'Expriment 5 - Image Fades'
     t.window.width = 1280
     t.window.height = 720
 
-    t.identity = t.window.title
+    t.identity = 'ca.taffer.love-experiments-5'
     t.version = 11.3 -- Made with LOVE 11.3 (Mysterious Mysteries).
     t.console = true
 end
