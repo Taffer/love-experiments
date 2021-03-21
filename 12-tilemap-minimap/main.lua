fe2a8995a1d3f80e5a044d03636a4ec884deefb7
Taffer <chrish@pobox.com>
2021-03-21 15:41:06 -0400

    Tilemap example, now with a minimap tracking the viewport

---
+++ 12-tilemap-minimap/main.lua
@@ -0,0 +1,164 @@
+-- Experiment 11 - Tilemap
+--
+-- By Chris Herborth (https://github.com/Taffer)
+-- MIT license, see LICENSE.md for details.
+
+local Class = require 'lib/middleclass'
+
+-- Map class
+local Map = Class('Map')
+
+function Map:initialize(mapData)
+    self.layers = mapData.layers
+
+    -- Texture atlas
+    self.atlas = love.graphics.newImage('resources/' .. mapData.tilesets[1].image)
+    self.atlasWidth = self.atlas:getWidth()
+    self.atlasHeight = self.atlas:getHeight()
+
+    self.quads = {}
+
+    -- Map constants.
+    self.mapWidth = mapData.width
+    self.mapHeight = mapData.height
+
+    self.tileWidth = mapData.tilesets[1].tilewidth
+    self.tileHeight = mapData.tilesets[1].tileheight
+    self.tileSpacing = mapData.tilesets[1].spacing
+
+    -- Create quads for the sprites on the atlas:
+    for y = 0, (self.atlasHeight / self.tileHeight) - 1 do
+        for x = 0, self.atlasWidth / self.tileWidth - 1 do
+            local atlas_x = x * self.tileWidth + x * self.tileSpacing
+            local atlas_y = y * self.tileHeight + y * self.tileSpacing
+            table.insert(self.quads, love.graphics.newQuad(atlas_x, atlas_y, self.tileWidth, self.tileHeight, self.atlas))
+        end
+    end
+end
+
+local first_done = false
+
+function Map:render(batch, view_x, view_y, view_width, view_height)
+    love.graphics.setColor(1, 1, 1, 1) -- white
+
+    -- We ignore layers for this experiment, there's only one.
+    local gameMap = self.layers[1].data
+
+    batch:clear()
+    for y = 0, view_height - 1 do
+        for x = 0, view_width - 1 do
+            local quad = gameMap[x + view_x + (y + view_y) * self.mapWidth + 1]
+            if not first_done then
+                first_done = true
+            end
+            if quad > 0  and quad < #self.quads then
+                batch:add(self.quads[quad], x * self.tileWidth, y * self.tileHeight)
+            end
+        end
+    end
+    batch:flush()
+
+    love.graphics.draw(batch)
+end
+
+-- All the stuff we've loaded already.
+gameResources = {
+    fonts = {},
+    images = {},
+    quads = {},
+
+    map = dofile('resources/map.lua'),
+}
+
+-- Current state of the game.
+gameState = {
+    view_x = 0, -- In tiles.
+    view_y = 0,
+    view_width = math.floor(love.graphics.getWidth() / gameResources.map.tilewidth),
+    view_height = math.floor(love.graphics.getHeight() / gameResources.map.tileheight),
+
+    batch = nil, -- SpriteBatch for drawing, created in love.load()
+}
+
+-- Love callbacks.
+function love.load()
+    math.randomseed(os.time())
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    io.stdout:setvbuf("no") -- Don't buffer console output.
+
+    local gameResources = gameResources
+    local gameState = gameState
+
+    gameResources.images.minimap = love.graphics.newImage('resources/mini-map.png')
+    gameResources.quads.minimap = love.graphics.newQuad(0, 0,
+        gameResources.images.minimap:getWidth(), gameResources.images.minimap:getHeight(), gameResources.images.minimap)
+    gameResources.fonts.variable = love.graphics.newFont('resources/fonts/LiberationSerif-Bold.ttf', 16)
+
+    -- Create the map.
+    gameResources.map = Map:new(gameResources.map)
+
+    -- Visible tiles * number of layers.
+    gameState.batch = love.graphics.newSpriteBatch(gameResources.map.atlas, gameState.view_width * gameState.view_height)
+end
+
+function love.draw()
+    local gameState = gameState
+
+    love.graphics.clear(0, 0, 0, 1)
+
+    gameResources.map:render(gameState.batch, gameState.view_x, gameState.view_y, gameState.view_width, gameState.view_height)
+
+    -- Render the minimap.
+    --
+    -- The map and minimap are both square, so we only have to keep track of
+    -- one delta factor.
+    local minimap_x = love.graphics.getWidth() - gameResources.images.minimap:getWidth()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(gameResources.images.minimap, gameResources.quads.minimap, minimap_x, 0)
+
+    local size_delta = gameResources.images.minimap:getWidth() / gameResources.map.mapWidth
+    local highlight_x = minimap_x + gameState.view_x * size_delta
+    local highlight_y = gameState.view_y * size_delta
+    local highlight_w = gameState.view_width * size_delta
+    local highlight_h = gameState.view_height * size_delta
+    love.graphics.setColor(1, 0, 1, 1)
+    love.graphics.rectangle('line', highlight_x, highlight_y, highlight_w, highlight_h)
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setFont(gameResources.fonts.variable)
+    love.graphics.print('Press arrow keys or WASD.', 10, 10)
+end
+
+-- Event generation.
+function love.keyreleased(key)
+    local gameResources = gameResources
+    local gameState = gameState
+
+    if gameResources.map == nil then
+        return
+    end
+
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'right' or key == 'd' then
+        gameState.view_x = gameState.view_x + 1
+        if gameState.view_x + gameState.view_width > gameResources.map.mapWidth then
+            gameState.view_x = gameResources.map.mapWidth - gameState.view_width
+        end
+    elseif key == 'left' or key == 'a' then
+        gameState.view_x = gameState.view_x - 1
+        if gameState.view_x < 0 then
+            gameState.view_x = 0
+        end
+    elseif key == 'down' or key == 's' then
+        gameState.view_y = gameState.view_y + 1
+        if gameState.view_y + gameState.view_height > gameResources.map.mapHeight then
+            gameState.view_y = gameResources.map.mapHeight - gameState.view_height
+        end
+    elseif key == 'up' or key == 'w' then
+        gameState.view_y = gameState.view_y - 1
+        if gameState.view_y < 0 then
+            gameState.view_y = 0
+        end
+    end
+end
